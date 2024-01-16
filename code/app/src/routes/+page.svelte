0f5d4d7e39a6908c4459b89b8ba7849e258a9a51
Alex Milde <alex@finekost.com>
2024-01-16 13:47:16 +0100

    #main updated packages

--- code/app/src/routes/+page.svelte
+++ code/app/src/routes/+page.svelte
@@ -62,7 +62,7 @@
     </div>
     <div class="col-span-10 col-start-2 mt-2 mb-4" class:opacity-20={!initialised}>
         <form method="POST" action="?/search" use:enhance={search}>
-            <input class="border px-2 py-2" bind:value={searchNeedle} name="search" />
+            <input class="border px-2 py-2" placeholder="Search" bind:value={searchNeedle} name="search" />
             <button>Search</button>
         </form>
         <div>

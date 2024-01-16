b927c3640c76e51a1d9c48dfd3ac0197456e1517
Alex Milde <alex@finekost.com>
2024-01-16 11:37:58 +0100

    #main try fresh deployment

--- code/app/src/routes/+page.svelte
+++ code/app/src/routes/+page.svelte
@@ -60,7 +60,7 @@
         {#if !initialised}Loading{/if}
         {#if loading}Loading{/if}
     </div>
-    <div class="col-span-10 col-start-2 mb-4" class:opacity-20={!initialised}>
+    <div class="col-span-10 col-start-2 mt-2 mb-4" class:opacity-20={!initialised}>
         <form method="POST" action="?/search" use:enhance={search}>
             <input class="border px-2 py-2" bind:value={searchNeedle} name="search" />
             <button>Search</button>

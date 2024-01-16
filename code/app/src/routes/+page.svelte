3b6373a0a410b0446a62be609136987cbbb99875
Alex Milde <alex@finekost.com>
2024-01-16 22:27:19 +0100

    #main minor layout fix

--- code/app/src/routes/+page.svelte
+++ code/app/src/routes/+page.svelte
@@ -118,7 +118,7 @@
             <div class="grid grid-cols-1 gap-4 sm:grid-cols-3 mt-8">
                 {#each searchResults as searchResult}
                     <div
-                        class="relative flex items-center space-x-3 rounded-lg border border-gray-300 bg-white px-6 pt-5 shadow-sm focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-400"
+                        class="relative flex items-center space-x-3 rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-400"
                     >
                         <div class="min-w-0 flex-1">
                             <a

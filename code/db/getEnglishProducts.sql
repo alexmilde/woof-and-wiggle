c30da60ca7c93b1ab309b0d2ef4fe2bb46d93b23
Alex Milde <alex@finekost.com>
2024-01-16 15:02:44 +0100

    #main updated db docs

--- code/db/getEnglishProducts.sql
+++ code/db/getEnglishProducts.sql
@@ -1,7 +1,24 @@
-SELECT p.id, p.post_title, p.post_content, p.guid
-FROM wp_posts p
-JOIN wp_icl_translations t ON p.ID = t.element_id
-WHERE t.language_code = 'en'
-and p.post_type like 'product%'
-and p.post_status = 'publish'
-and p.post_content != ''
+SELECT
+    p.id,
+    p.post_title,
+    p.post_content,
+    #tr.term_taxonomy_id,
+    p.guid
+FROM
+    wp_posts p
+JOIN
+    wp_icl_translations t ON p.ID = t.element_id
+LEFT JOIN
+    wp_term_relationships tr ON p.ID = tr.object_id
+WHERE
+    t.language_code = 'en'
+    AND p.post_type LIKE 'product%'
+    AND p.post_status = 'publish'
+    AND p.post_content != ''
+    AND NOT EXISTS (
+        SELECT 1
+        FROM wp_term_relationships tr_check
+        WHERE tr_check.object_id = p.ID
+            AND tr_check.term_taxonomy_id IN (450, 451, 9) #ausverkauft #sold-out #outofstock
+    )
+group by p.id
